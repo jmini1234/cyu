c56634f4900587979c12a4a53251ed1380ad1317
jmini1234 <shjj09@naver.com>
2019-01-25 15:50:08 +0900

    login_auth 변경

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,12 @@ class ApplicationController < ActionController::Base
     redirect_to :signin unless user_signed_in?
   end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    rescue ActiveRecord::RecordNotFound
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  #   rescue ActiveRecord::RecordNotFound
+  # end
+ def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
     def user_signed_in?
     # converts current_user to a boolean by negating the negation
